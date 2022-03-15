08d548a85afcdac6c4859eac88ef46bf5e52f90e
Francis Adewale <francisadewale1992@gmail.com>
2022-03-15 16:01:52 +0000

    can animate logo based on conditon, may need to use Gatsby in to make app an MPA instead of SPA

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import './App.css';
 import firebase from './firebase';
-import Home from './componenets/Home'
+import Home from './componenets/Home';
+import Login from "./componenets/Login"
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 function App() {
+
+  const [vegan, setVegan] = useState(false)
+  const [shake, setShake] = useState(false);
+
+
+  let result = JSON.parse(localStorage.getItem('isVegan'))
+  console.log(result)
+
   const db = firebase.firestore
   const auth = firebase.auth
+
+
+  function isVegan() {
+
+    if(vegan) {
+      localStorage.setItem("isVegan", JSON.stringify(vegan))
+      
+    } else {
+         setShake(true);
+         setTimeout(() => setShake(false), 2000);
+    }
+  }
+
+  function checkIfVegan(e) {
+    const {type, checked} = e.target
+    setVegan(checked)
+  }
 
   return (
     <div className='outer'>
       <div className='middle'>
         <div className='inner'>
-        <Home />
-        </div>
+          
+         <Home 
+        callback={isVegan}
+        handleChange={checkIfVegan}
+        shake={shake}
+        isVegan={vegan}
+         /> 
 
+        </div>
       </div>
 
     </div>
--- src/componenets/Home.js
+++ src/componenets/Home.js
@@ -1,16 +1,22 @@
 import React from "react";
 import logo from "../imgs/vescoverer.png"
 
-export default function Home() {
+
+export default function Home(props) {
+    
     return (
         <div>
-            <img className="home-logo" src={logo} alt="logo"/>
+            <img className={props.shake ? "shake" : "home-logo" } src={logo} alt="logo"/>
             <h3 className="home-question">Are You Vegan?</h3>
             <label className="switch">
-                <input type="checkbox"/>
+                <input 
+                type="checkbox"
+                checked={props.isVegan}
+                onChange={(e) => props.handleChange(e)}
+                />
                 <span className="slider round"></span>
             </label>
-            <button className="home-done-btn">Done</button>
+            <button className="home-done-btn" onClick={props.callback}>Done</button>
         </div>
 
     );
