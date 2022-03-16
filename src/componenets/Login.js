e36b7dd2ebda7eb580ac968419a7b57cf6284ff6
Francis Adewale <francisadewale1992@gmail.com>
2022-03-16 17:12:59 +0000

    add google sign in api/ finish login/signup page

--- src/componenets/Login.js
+++ src/componenets/Login.js
@@ -1,14 +1,28 @@
 import React from "react"
 import "../Login.css"
+import GoogleLogin from 'react-google-login';
+
 
 
 export default function Login() {
+
+    const responseGoogle = (response) => {
+        console.log(response);
+      }
+
     return (
             <div className='login-outer'>
                 <nav>Vescoverer</nav>
 
                 <div className='login-middle'>
                     <div className='login-inner'>
+                    <GoogleLogin
+                    clientId="932286838218-cie8hb1a9ev1l2b9u4cgock1i7mphmfn.apps.googleusercontent.com"
+                    buttonText="Login / Sign up"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                    />
                     </div>
                 </div>
             </div>
